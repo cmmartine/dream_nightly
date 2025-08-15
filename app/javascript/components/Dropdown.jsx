57bff34eb152410c25d003239c460978eb50e2ee
cmmartine <cmmartine@gmail.com>
2025-08-15 10:19:03 -0400

    Refactor Dropdown, various styling changes to the dropdown, dream form and page

--- app/javascript/components/Dropdown.jsx
+++ app/javascript/components/Dropdown.jsx
@@ -5,36 +5,35 @@ import { logout } from "../api/usersApi";
 export default function Dropdown() {
   const [openDropdown, setOpenDropdown] = useState(false);
 
-  if(!openDropdown) {
     return(
-      <nav className="dropdown dropdown-closed">
+      !openDropdown &&
+        <nav className="dropdown dropdown-closed">
+          <div className="dropdown-btn-container">
+            <button className="dropdown-btn" onClick={(e) => {
+              e.preventDefault();
+              setOpenDropdown((prev) => !prev);
+            }}>
+              <span className="hamburger-icon-line top-line"/>
+              <span className="hamburger-icon-line"/>
+              <span className="hamburger-icon-line"/>
+            </button>
+          </div>
+        </nav>
+      ||
+      <nav className="dropdown dropdown-open">
         <div className="dropdown-btn-container">
           <button className="dropdown-btn" onClick={(e) => {
             e.preventDefault();
-            setOpenDropdown(!openDropdown);
-          }}>
-            <span className="hamburger-icon-line top-line"/>
-            <span className="hamburger-icon-line"/>
-            <span className="hamburger-icon-line"/>
-          </button>
-        </div>
-      </nav>
-    );
-  } else {
-    return(
-      <nav className="dropdown">
-        <div className="dropdown-btn-container">
-          <button className="dropdown-btn" onClick={(e) => {
-            e.preventDefault();
-            setOpenDropdown(!openDropdown);
+            setOpenDropdown((prev) => !prev);
           }}>
             <span className="hamburger-close-icon-line">X</span>
           </button>
         </div>
-        <button className="logout-btn" onClick={() => {
-          logout();
-        }}>Log Out</button>
+        <div className='dropdown-items-container'>
+          <button className="logout-btn" onClick={() => {
+            logout();
+          }}>Log Out</button>
+        </div>
       </nav>
     );
-  }
 };
