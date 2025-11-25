067b560b73345591a68b4db8fc85e4325d07db6d
cmmartine <cmmartine@gmail.com>
2025-11-25 06:35:37 -0500

    Update landing page

--- app/javascript/components/LandingPage.jsx
+++ app/javascript/components/LandingPage.jsx
@@ -7,10 +7,24 @@ export default function LandingPage() {
       <div className='info-page-row'>
         <h2 className='info-text info-call-to-action'>{appDescription.callToAction}</h2>
         <p className='info-text'>{appDescription.body}</p>
-        <div className='app-screenshot'/>
+        <div className='screenshot app-screenshot'/>
+      </div>
+      <div className='info-page-row'>
+        <h3 className='info-text'>{appDescription.calendar.title}</h3>
+        <div className='screenshot calendar-screenshot'/>
+        <p className='info-text'>{appDescription.calendar.text}</p>
+      </div>
+
+      <div className='info-page-row'>
+        <h3 className='info-text'>{appDescription.search.title}</h3>
+        <div className='screenshot search-screenshot'/>
+        <p className='info-text'>{appDescription.search.text}</p>
+      </div>
+      <div className='info-page-row'>
         <p className='info-text info-text-bottom'>{appDescription.body2}</p>
         <p className='info-text info-text-bottom'>{appDescription.body3}</p>
       </div>
+
     </div>
   );
 };
--- app/javascript/constants/appInfo.js
+++ app/javascript/constants/appInfo.js
@@ -5,6 +5,14 @@ export const appDescription = {
   body: "Your dreams fade fast - but recording them helps you remember, reflect, and uncover patterns in your subconscious. Whether you're chasing creativity, clarity, or curiosity, this journal helps you turn fleeting moments into lasting insight.",
   body2: "No downloads. No clutter. Just open your browser and start logging.",
   body3: "Sign up now and see what your mind's been trying to tell you.",
+  calendar: {
+    title: "Navigate your dream history",
+    text: "Use the calendar to move through days, weeks, or months. Instantly see which nights hold recorded dreams and jump back to revisit them.",
+  },
+  search: {
+    title: "Find your past dreams",
+    text: "Quickly bring old entries back into focus. Whether it's a vivid night you want to revisit or a recurring theme you're curious about, search makes it easy to rediscover.",
+  }
 }
 
 export const noDreams = "No dream entries recorded for this night.";
